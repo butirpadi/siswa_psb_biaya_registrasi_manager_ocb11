c6e56002ddb981b873df83bd31d54434e3308fe8
Butirpadi <butirpadi@gmail.com>
2018-07-15 18:25:58 +0700

    First Commit

---
+++ __manifest__.py
@@ -0,0 +1,36 @@
+# -*- coding: utf-8 -*-
+{
+    'name': "Biaya Registrasi PSB Manager",
+
+    'summary': """
+        Biaya Registrasi PSB Manager for Siswa Module""",
+
+    'description': """
+        Biaya Registrasi PSB Manager for Siswa Module
+    """,
+
+    'author': "Tepat Guna Karya",
+    'website': "http://www.tepatguna.id",
+
+    # Categories can be used to filter modules in modules listing
+    # Check https://github.com/odoo/odoo/blob/master/odoo/addons/base/module/module_data.xml
+    # for the full list
+    'category': 'Education',
+    'version': '1.0',
+
+    # any module necessary for this one to work correctly
+    'depends': ['base'],
+
+    # always loaded
+    'data': [
+        # 'security/ir.model.access.csv',
+        'views/views.xml',
+        'views/templates.xml',
+    ],
+    # only loaded in demonstration mode
+    'demo': [
+        'demo/demo.xml',
+    ],
+    'installable': True,
+    'application': True,
+}
